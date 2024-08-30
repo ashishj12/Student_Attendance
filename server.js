644d086dfd6f9505fbf85829e6fb0160d2a5d65a
ashishj12 <jangidashish38@gmail.com>
2024-08-30 19:54:33 +0530

    Apply auth middleware to attendance routes

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const attendanceRoutes = require("./routes/attendanceRoutes");
+const authRoutes = require("./routes/authRoutes");
+const authMiddleware = require("./middlware/authMiddleware");
 const PORT = 5000;
 const app = express();
 
@@ -9,14 +11,15 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Connect to MongoDB with error handling
+// Connect to Mongodb with error handling
 mongoose
   .connect("mongodb://localhost/student-attendance")
   .then(() => console.log("Connected to MongoDB"))
   .catch((error) => console.error("Could not connect to MongoDB:", error));
 
 // Use routes
-app.use("/api/attendance", attendanceRoutes);
+app.use("/api/attendance", authMiddleware, attendanceRoutes); // Apply auth middleware to attendance routes
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
