de21c63a62e4a7900bb948b245168ede83d4cbe6
ashishj12 <jangidashish38@gmail.com>
2024-08-31 14:02:42 +0530

    auth router login signup

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,27 +2,27 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
-
-const JWT_SECRET = "key@123";
+const JWT_SECRET = process.env.JWT_SECRET;
 
 // Register new student
 router.post("/register", async (req, res) => {
   try {
     const { username, email, password } = req.body;
     if (!username || !email || !password) {
-      return res.status(400).send("All fields are required");
+      return res.status(400).json({ message: "All fields are required" });
     }
 
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.status(400).send("Email already in use");
+      return res.status(400).json({ message: "Email already in use" });
     }
-
     const user = new User({ username, email, password });
     await user.save();
-    res.status(201).send("User registered successfully");
+    res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
-    res.status(500).send("Internal Server Error: " + error.message);
+    res
+      .status(500)
+      .json({ message: "Internal Server Error: " + error.message });
   }
 });
 
@@ -31,28 +31,30 @@ router.post("/login", async (req, res) => {
   try {
     const { username, password } = req.body;
     if (!username || !password) {
-      return res.status(400).send("All fields are required");
+      return res.status(400).json({ message: "All fields are required" });
     }
 
     const user = await User.findOne({ username });
     if (!user) {
-      return res.status(404).send("User not found");
+      return res.status(404).json({ message: "User not found" });
     }
 
     const isMatch = await user.comparePassword(password);
     if (!isMatch) {
-      return res.status(400).send("Invalid password");
+      return res.status(400).json({ message: "Invalid password" });
     }
 
     const token = jwt.sign(
       { id: user._id, username: user.username },
-      JWT_SECRET,
+      process.env.JWT_SECRET,
       { expiresIn: "1h" }
     );
 
     res.status(200).json({ token });
   } catch (error) {
-    res.status(500).send("Internal Server Error: " + error.message);
+    res
+      .status(500)
+      .json({ message: "Internal Server Error: " + error.message });
   }
 });
 
