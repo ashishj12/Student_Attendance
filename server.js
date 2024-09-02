06049d47aca0914d2b8b433f77442a2ad315ab24
ashishj12 <jangidashish38@gmail.com>
2024-09-02 21:51:54 +0530

    add root route

--- server.js
+++ server.js
@@ -25,6 +25,12 @@ mongoose
 app.use("/api/attendance", authMiddleware, attendanceRoutes); // Apply auth middleware to attendance routes
 app.use("/api/auth", authRoutes);
 
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
